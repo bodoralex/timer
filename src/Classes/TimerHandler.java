d2740bae974c6f03a924c19e28469d1a9f65f7d7
Bodor Alex <bodaleaa@gmail.com>
2017-06-01 17:51:52 +0200

    a missing break added

--- src/Classes/TimerHandler.java
+++ src/Classes/TimerHandler.java
@@ -109,6 +109,7 @@ public class TimerHandler implements Runnable {
                     break;
                 case "exit":
                     exit();
+                    break;
                 default:
                     System.err.println("Invalid statement");
             }
